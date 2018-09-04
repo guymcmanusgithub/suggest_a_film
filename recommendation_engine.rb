92f4e60767c88f1997f286c36d4ff1237c84fff4
Athoi <athoi@Athois-MacBook-Pro.local>
2018-09-04 12:40:13 +1000

    Determined the number of times a film was rated

--- recommendation_engine.rb
+++ recommendation_engine.rb
@@ -4,6 +4,9 @@ require  'csv'
 movie_names = CSV.read("/Users/athoi/apps/ruby/excercises/suggest_a_film/movies.csv")
 all_user_rating = CSV.read("/Users/athoi/apps/ruby/excercises/suggest_a_film/ratings.csv")
 
+## remove the fields
+movie_names.shift
+all_user_rating.shift
 
 ## Remove the timestamp from each row using pop as it is the last element
 user_data = []
@@ -20,4 +23,24 @@ movie_names.each do |rows|
 end
 
 # movie data is the movie list without the genres
-p movie_data
+# p movie_data.length
+# p user_data.length
+
+# find out the movies that have been rated the most often
+movies_rated_often = []
+user_data.each do |ratings|
+  movies_rated_often << ratings[1]
+end
+# p movies_rated_often.sort
+
+most_rated_movies = Hash.new(0)
+
+movies_rated_often.each do |movie|
+  most_rated_movies[movie] += 1
+end
+
+most_rated_movies.each do |k, v|
+  hashmovies = "movieID #{k} was rated #{v} times \n"
+  print hashmovies
+end
+
