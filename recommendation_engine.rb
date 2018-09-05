5cab5a5236fb96e98aa1e9c8d9552df72f26406d
Athoi <athoi@Athois-MacBook-Pro.local>
2018-09-05 16:48:34 +1000

    refactored code and edited comments

--- recommendation_engine.rb
+++ recommendation_engine.rb
@@ -1,23 +1,29 @@
-require  'csv'
+##########  LOADING ALL THE GEMS #########
+require 'csv'
 require 'pearson'
 require 'tty-spinner'
 require 'colorize'
 spinner = TTY::Spinner.new("[:spinner] Loading your recommendations...", format: :pulse_2)
 # spinner = TTY::Spinner.new("wait :spinner ...", format: :spin_2)
-# loading all the required files from csv files
+
+##########  LOADING THE CSV FILES FROM MOVIELENS #########
 movie_names = CSV.read("/Users/athoi/apps/ruby/excercises/suggest_a_film/movies.csv")
 all_user_rating = CSV.read("/Users/athoi/apps/ruby/excercises/suggest_a_film/ratings.csv")
+
+#The number of individual users within the dataset.  Will be used to iterate over the data.
 number_of_users_in_db = 671
-## remove the fields
+
+## remove the field names/headers from the first entry within the csv
 movie_names.shift
 all_user_rating.shift
 
 ## Remove the timestamp from each row using pop as it is the last element
-user_data = []    # user data is the user ratings without the timestamp
+user_data = []    
 all_user_rating.each do |rows|
   rows.pop
   user_data << rows
 end
+# user data is now the user ratings without the timestamp
 
 movie_data = []   # movie data is the movie list without the genres
 movie_names.each do |rows|
@@ -25,17 +31,17 @@ movie_names.each do |rows|
   movie_data << rows
 end
 
+# p movie_data.length #Used for testing
+# p user_data.length #Used for testing
 
-# p movie_data.length
-# p user_data.length
+######### GETTING TOP 100 MOST RATED MOVIES FOR USER TO RATE FROM ##########
 
-# find out the movies that have been rated the most often
+# find out the movies that have been rated most often
 movies_rated_often = []
 user_data.each do |ratings|
   movies_rated_often << ratings[1]
 end
 # p movies_rated_often.sort
-
 most_rated_movies = Hash.new(0)
 
 movies_rated_often.each do |movie|
@@ -43,20 +49,20 @@ movies_rated_often.each do |movie|
 end
 sorted_most_rated_movies = most_rated_movies.sort_by{|k, v| v}.reverse
 
-# print sorted_movies
-# p most_rated_movies
+# p sorted_movies #Used for testing
+# p most_rated_movies #Used for testing
 
-sorted_most_rated_movies.each do |k, v|
-  hashmovies = "movieID #{k} was rated #{v} times \n"
-  # print hashmovies
-end
+# FOR TESTING!!! (Gretch made us do this)
+# sorted_most_rated_movies.each do |k, v|
+#   hashmovies = "movieID #{k} was rated #{v} times \n"
+#   # print hashmovies
+# end
 
-## the top hundred movies
+# saving the hundred most rated movies
 top_hundred_movies = sorted_most_rated_movies[1..100]
 
-# match movie ids in movie_data with movie ids in top_100list
-# enter the title inside the top_100 array 
-
+# match movie ids in movie_data with movie ids in top_hundred_movies variable 
+# and enter the title inside the top_100 array 
 movie_data.each do |item|
   top_hundred_movies.each do |movie|
     if item[0] == movie[0]
@@ -65,20 +71,23 @@ movie_data.each do |item|
   end  
 end 
 
-# p top_hundred_movies
+# p top_hundred_movies #Used for testing
 
-## Generate randomly selected 5 or 10 movies for the user to rate
+######### USER INTERFACE ########
+
+# Generate randomly selected movies for the user to rate from the top_hundred_movies variable
 movies_and_ratings = []
-puts "How many movies would you like to review? \n(keeping in mind that the more you review the more accurate our recommendations)"
+print "How many movies would you like to review? \n(keep in mind that the more movies you review the more accurate our recommendations will be): "
 number_of_movies_to_rate = gets.chomp.to_i
-puts "please rate the #{number_of_movies_to_rate} movies below:"
+puts "Please rate the #{number_of_movies_to_rate} movies below:"
 
 counter = 0
 while counter < number_of_movies_to_rate
-  current_movie_to_rate = top_hundred_movies[rand(100)] # this generates random movies from the to_100 array
-  current_movie_title = current_movie_to_rate[2]
-  current_movie_id = current_movie_to_rate[0]
-  print "please rate #{current_movie_title.blue} from 1 to 5: "
+  current_movie_to_rate = top_hundred_movies[rand(100)] # this generates random movies from the top_hundred_movies array
+  current_movie_title = current_movie_to_rate[2] #Saves the movie title in the current_movie_title variable 
+  current_movie_id = current_movie_to_rate[0] #Saves the movie id in the variable current_movie_id
+  print "Please rate #{current_movie_title.blue} from 1 to 5: "
+  #Following lines of code get user responses and store them in a hash, while assigning the user with the userID '5000'
   current_user_rating = gets.chomp.to_f
   rating_hash = {
     user_id: "5000",
@@ -86,52 +95,50 @@ while counter < number_of_movies_to_rate
     movie_title: current_movie_title,
     user_rating: current_user_rating
   }
-  movies_and_ratings << rating_hash
+  movies_and_ratings << rating_hash #This pushes the users ratings into the movies_and_ratings array
   counter += 1
 end
+# print movies_and_ratings # Used for testing
 
-spinner.auto_spin
+#### LOADING TTY:Spinner icon while the program calculates the recommendations
+spinner.auto_spin 
 
-puts
-# print movies_and_ratings
-puts
+####### CREATING A HASH FROM USER INPUTS TO USE WITH THE PEARSON GEM #######
 movies_ids = []
 movies_ratings = []
 movies_and_ratings.each do |hash|
   movies_ids << hash[:movie_id]
   movies_ratings << hash[:user_rating]
 end
-user_5000_hash = movies_ids.zip(movies_ratings).to_h
+user_5000_hash = movies_ids.zip(movies_ratings).to_h 
+#The .zip method combines the two arrays (of same length). Then we convert into one hash
 user_5000_hash_for_pearsons = {rating_hash[:user_id] => user_5000_hash}
-# p user_5000_hash_for_pearsons
-# print movies_ids
-# puts
-# print movies_ratings
 
+# p user_5000_hash_for_pearsons #Used for testing
+# print movies_ids #Used for testing
+# print movies_ratings #Used for testing
+# print user_data #Used for testing
 
-# print user_data
+######### CREATING A HASH FROM MOVIELENS DATA TO MATCH THE STRUCTURE REQUIRED BY THE PEARSON GEM ######
 def create_hash_of_movieid_and_ratings_for_each_user(user_data_array,userid)
   all_movies_rated_by_each_user = []
   all_ratings_by_each_user = []
   user_data_array.each do |item|
     if item[0] == userid
-      # print item,index
-      all_movies_rated_by_each_user << item[1]
-      all_ratings_by_each_user << item[2].to_f # changed the ratings to floats to use the pearson gem
+      # print item,index #Used for testing
+      all_movies_rated_by_each_user << item[1] # pushes movieId into an array 
+      all_ratings_by_each_user << item[2].to_f # changes the ratings to floats to use the pearson gem
     end
   end
   user_hash = all_movies_rated_by_each_user.zip(all_ratings_by_each_user).to_h
-  user_hash
+  user_hash #returns the newly merged hash of the hashes listed above
 end
 
-### PROBLEMS : found out that in ruby you cannot use a variable in a mthod 
-### that is outside of the method without passing it in the argument
-### in this case it is the variable "user_data"
 counter = 1
-array_of_each_user = [] ## used the method in a loop to create hashes for all the users in the ratings.csv file
-while counter <= number_of_users_in_db # usernumber in the dataset is 671
+array_of_user_hashes = [] ## used the method create_hash_of_movieid_and_ratings_for_each_user in a loop to create hashes for all the users in the ratings.csv file
+while counter <= number_of_users_in_db 
   user = create_hash_of_movieid_and_ratings_for_each_user(user_data, "#{counter}")
-  array_of_each_user << user
+  array_of_user_hashes << user
   counter += 1
 end
 counter = 1
@@ -140,20 +147,19 @@ while counter <= number_of_users_in_db
   array_of_id_of_user << "#{counter}"
   counter += 1
 end
-##
-# p array_of_each_user
-# p array_of_id_of_user
-scores_users = array_of_id_of_user.zip(array_of_each_user).to_h
+# p array_of_user_hashes # Used for testing
+# p array_of_id_of_user # Used for testing
+scores_users = array_of_id_of_user.zip(array_of_user_hashes).to_h
+# p scores_users # Used for testing
+
+########## ADDING USER INPUT HASH TO THE HASH OF ALL THE OTHER USERS #########
 combined_hash = scores_users.merge(user_5000_hash_for_pearsons)
-# p combined_hash
+# p combined_hash # Used for testing
+# p Pearson.coefficient(combined_hash, '5000', '4') # Used for testing
+# p Pearson.closest_entities(combined_hash, '5000', limit: 5) # Used for testing
 
-# # p scores_users
-
-# p Pearson.coefficient(combined_hash, '5000', '4')
-# puts
-# p Pearson.closest_entities(combined_hash, '5000', limit: 5) 
-
-recommended_movies = Pearson.recommendations(combined_hash, '5000')
+########### CREATING MOVIE RECOMMENDATIONS FOR USER ###########
+recommended_movies = Pearson.recommendations(combined_hash, '5000') # '5000' is the assigned userId to the user
 recommended_list = []
 recommended_movies.each do |movie_id|
   movie_data.each do |movie|
@@ -162,7 +168,7 @@ recommended_movies.each do |movie_id|
     end
   end
 end
-puts
+# puts
 spinner.stop("\nWe recommend these movies to you: \n")
 puts recommended_list
 
